be11a54e1bfac3a85ac4aa1c9c53eebcb9aad56a
Guillaume Arp <guillaume.arp@gmail.com>
2022-06-09 17:27:47 +0200

    added file and plot

--- Guillaume/Streamlit/pages/F1_Season_2022.py
+++ Guillaume/Streamlit/pages/F1_Season_2022.py
@@ -30,6 +30,76 @@ st.set_page_config(
 
 # Functions
 
+#Récupération des noms et des couleurs à partir d'une course
+
+@st.cache()
+def plot_champ_pos():
+    '''
+    Plots the evolution of the drivers standings
+    '''
+    drv = pd.read_csv('../data/drivers_standings.csv', index_col=0)
+    drivers_info = pd.read_csv('../data/drivers_info.csv', index_col=0)
+    
+    df_class = drivers_info.merge(drv, how='right', left_on = ['Abbreviation'], right_index = True).reset_index()
+
+    nb = len(drv.transpose())+1
+    df_final = pd.DataFrame(columns=['DriverNumber', 'BroadcastName', 'Abbreviation', 'TeamName', 'TeamColor', 'FirstName', 'LastName', 'FullName','Points', 'Race'])
+    for i in range(1,nb):
+        df_class_ligne = df_class.loc[:,['DriverNumber', 'BroadcastName', 'Abbreviation', 'TeamName', 'TeamColor', 'FirstName', 'LastName', 'FullName',str(i)]]
+        df_class_ligne['Race'] = i
+        df_class_ligne.rename(columns={str(i): 'Points'}, inplace = True)
+        df_final = pd.concat([df_final, df_class_ligne])
+    df_final.reset_index(drop = True, inplace = True)
+
+    df_final = df_final.sort_values(by=['Race', 'Points'], ascending = [True, False])
+    df_final['classement']=len(drv)
+    longueur = len(df_final)
+    df_final.iloc[0,10] = 1
+
+    for i in range(1,longueur -1):
+        if df_final.iloc[i,9] == df_final.iloc[i-1,9]:
+            df_final.iloc[i,10] = df_final.iloc[i-1,10] +  1
+        else :
+            df_final.iloc[i,10] = 1
+
+    df_init = df_final[df_final['Race']==nb-1].copy()
+    df_init['Race'] = 0
+    df_init['Points'] = 0
+
+    df_final = pd.concat([df_final, df_init])
+            
+    colorMap ={}
+    df_class = pd.DataFrame(df_class)
+    for i in df_class.itertuples() :
+        colorMap[i.Abbreviation] = '#' + i.TeamColor
+            
+    df_final = df_final.sort_values(by=['Race', 'classement'], ascending = [True, True])
+    maxY = df_final['Points'].max() + 20
+    
+    fig = px.bar(df_final, x="Abbreviation", y="Points",  color = "Abbreviation", color_discrete_map = colorMap, animation_frame="Race", 
+                labels=dict(Abbreviation="Name", classement="Ranking", FullName = "Pilot"), width = 1000, height=800, text = df_final['classement'])
+
+    fig.update_layout(title_text='Evolution of points in the championship', title_x=0.5, transition = {'duration': 1000}, showlegend = False)
+    fig.update_traces(textposition='inside', hovertemplate='Points: %{y}' )
+    fig.update_yaxes(range=[0, maxY])
+
+    return fig
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @st.cache(allow_output_mutation=True)
 def get_drivers_standings():
     return pd.read_csv('data/drivers_standings.csv', index_col=0)
