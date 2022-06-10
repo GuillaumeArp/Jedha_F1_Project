63dc28b228ea8e7bbfa24945f31bfdca273d907e
Berenger <bqueune@hotmail.com>
2022-06-10 12:34:43 +0200

    update

--- Berenger/Streamlit/pages/Session_Analysis.py
+++ Berenger/Streamlit/pages/Session_Analysis.py
@@ -42,7 +42,7 @@ events_list['CountryAbbreviation'] = country_abbrev
 session_dict = {'conventional': ['Practice 1', 'Practice 2', 'Practice 3', 'Qualifying', 'Race'],
                 'sprint': ['Practice 1', 'Qualifying', 'Practice 2', 'Sprint', 'Race']}
 
-driver_dict = {
+visualisations_dict = {
         'VER': "Max Verstappen",
         "SAI": "Carlos Sainz",
         'MSC': "Mick Schumacher",
@@ -406,6 +406,7 @@ def format_results_race(session_type):
         
         # Drop unnecessary columns
         results_formatted.drop(columns=['Status', 'Time', 'TimeStr', 'TimeDifference'], inplace=True)
+        return results_formatted
         
     elif session_type == 'Qualifying':
         results_formatted = pd.DataFrame(results.copy())
@@ -414,11 +415,64 @@ def format_results_race(session_type):
         temp_q2 = format_time(results['Q2'], 11)
         results_formatted['Q2_time'] = temp_q2
         temp_q3 = format_time(results['Q3'], 11)
-        results_formatted['Q3_time'] = temp_q3
-        results_formatted = results_formatted[['Name','TeamName','Position','Q1_time','Q2_time','Q3_time']]
+        results_formatted['Q3_time'] = temp_q3        
+        results_formatted = results_formatted[['FullName','TeamName','Position','Q1_time','Q2_time','Q3_time']]
+        results_formatted = results_formatted.rename(columns = {'FullName': 'Name'})
         results_formatted['Position'] = results_formatted['Position'].astype(int)
-            
-    return results_formatted   
+        return results_formatted 
+
+def fastest_lap_comparison(fastest_laps):
+    # Pass session.laps.pick_fastest() as argument when calling the function
+    '''
+    Plots the comparison of the best lap times of the selected session
+    '''
+    drivers = pd.unique(session.laps['Driver'])
+
+    list_fastest_laps = []
+    for drv in drivers:
+        drvs_fastest_lap = session.laps.pick_driver(drv).pick_fastest()
+        list_fastest_laps.append(drvs_fastest_lap)
+    fastest_laps = Laps(list_fastest_laps).sort_values(by='LapTime').reset_index(drop=True)
+
+    pole_lap = fastest_laps.pick_fastest()
+    fastest_laps['LapTimeDelta'] = fastest_laps['LapTime'] - pole_lap['LapTime']
+
+    pole_lap = fastest_laps.pick_fastest()
+    fastest_laps['LapTimeDelta'] = fastest_laps['LapTime'] - pole_lap['LapTime']
+    
+    teamcol = {}
+    df_results = pd.DataFrame(session.results)
+
+    for i in df_results.itertuples():
+        teamcol[i.Abbreviation] = '#' + i.TeamColor
+
+    fastest_laps = fastest_laps.dropna(subset=['Time'])
+
+    timestr = format_time(fastest_laps['LapTimeDelta'],13)
+    timelap = format_time(fastest_laps['LapTime'],11)
+
+    fastest_laps['Delta']=timestr
+    fastest_laps['BestLapstr']=timelap
+    fastest_laps['Delta']=fastest_laps['Delta'].apply(lambda x : x + ' sec' )
+    fastest_laps['BestLapstr']=fastest_laps['BestLapstr'].apply(lambda x : x + ' sec' )
+
+    plot_title = f"{session.event.year} {session.event.EventName} - {session.name} - Fastest Lap: {fastest_laps['BestLapstr'].iloc[0]} - {fastest_laps['Driver'].iloc[0]}"
+
+    fig = px.bar(fastest_laps, 
+                x="LapTimeDelta", 
+                y="Driver", 
+                color='Driver',
+                color_discrete_map=teamcol ,
+                orientation='h',
+                width=1000, height=600,
+                template='plotly_dark',
+                hover_data={'Delta':True,'LapTimeDelta':False})
+
+    fig.update_layout(showlegend=False, title_text=plot_title)
+    fig.update_xaxes(visible=False)
+    fig.update_yaxes(categoryorder='total descending')
+
+    return fig
 
 @st.cache(allow_output_mutation=True)
 def load_data_session(year, gp_round, ses):
@@ -483,12 +537,21 @@ if gp_round is not None:
     lap_1, lap_2 = get_telemetry_data(fastest_driver_1, fastest_driver_2)
 
 
+
+
+
     col1, col2, col3, col4, col5, col6 = st.columns([1, 1, 1, 1, 1, 1])
     with col2:
-        visualisation_1 = st.selectbox("Visualisation 1", ("Drivers speed comparison", "Session results"), key = 1)
+        if ses == 'Qualifying' or ses == 'Race' or ses == 'Sprint':
+            visualisation_1 = st.selectbox("Visualisation 1", ("Drivers speed comparison", "Session results", "Fastest laps"), key = 1)
+        else:
+            visualisation_1 = st.selectbox("Visualisation 1", ("Drivers speed comparison", "Fastest laps"), key = 1)
 
     with col5:
-        visualisation_2 = st.selectbox("Visualisation 2", ("Drivers speed comparison", "Session results"), key = 2)
+        if ses == 'Qualifying' or ses == 'Race' or ses == 'Sprint':
+            visualisation_2 = st.selectbox("Visualisation 2", ("Drivers speed comparison", "Session results", "Fastest laps"), key = 2)
+        else:
+            visualisation_2 = st.selectbox("Visualisation 2", ("Drivers speed comparison","Fastest laps"), key = 2)
     
     col1, col2, col3, col4, col5, col6 = st.columns([1, 15, 1, 1, 15, 1])
 
@@ -496,15 +559,30 @@ if gp_round is not None:
     with col2:
         if visualisation_1 == "Drivers speed comparison":
             st.plotly_chart(plot_stacked_data(session, car_data_1, car_data_2, driver_1, driver_2, ref_tel, delta_time), use_container_width=True)
-        elif visualisation_2 == "Session results":
+        elif visualisation_1 == "Session results":
+            st.write("")
+            st.write("")
+            st.markdown("<h5 style='text-align: center; color: white;'>Session results</h5>", unsafe_allow_html=True)
+            st.write("")
+            st.write("")
             st.dataframe(format_results_race(ses))
+        elif visualisation_1 == "Fastest laps":
+            st.plotly_chart(fastest_lap_comparison(session.laps.pick_fastest()))
 
     with col5:
         if visualisation_2 == "Drivers speed comparison":
             st.plotly_chart(plot_stacked_data(session, car_data_1, car_data_2, driver_1, driver_2, ref_tel, delta_time), use_container_width=True)
         elif visualisation_2 == "Session results":
-            format_results_race(ses)
-    
+            st.write("")
+            st.write("")
+            st.markdown("<h5 style='text-align: center; color: white;'>Session results</h5>", unsafe_allow_html=True)
+            st.write("")
+            st.write("")
+            st.dataframe(format_results_race(ses))
+        elif visualisation_2 == "Fastest laps":
+            st.plotly_chart(fastest_lap_comparison(session.laps.pick_fastest()))
+
+
         
 
             
